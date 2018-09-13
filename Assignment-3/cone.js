00a88749e892101118c0dfacf42408056202329a
Tristan <31672599+ttcesena@users.noreply.github.com>
2018-09-12 19:40:01 -0700

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -2,17 +2,17 @@ var gl = null;
 var cone = null;
 
 function init() {
+    
     var canvas = document.getElementById( "webgl-canvas" );
 
+    
     gl = WebGLUtils.setupWebGL( canvas );
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
         return;
     }
-    
-    cone = new Cone(gl,n)
-
+    cone = new Cone(gl, 10);
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
 
     render();
@@ -24,3 +24,4 @@ function render() {
 }
 
 window.onload = init;
+
